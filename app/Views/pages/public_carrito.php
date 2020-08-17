27a0ac78c94ae63a81c7976de7c802f61ec0c33f
nickienicks <69782181+nickienicks@users.noreply.github.com>
2020-08-17 18:49:44 -0500

    mis cambios

--- app/Views/pages/public_carrito.php
+++ app/Views/pages/public_carrito.php
@@ -1,3 +1,94 @@
-<div class=container>
+
+<div class="container">
+        
+              <table class="responsive-table highlight centered">
+                <thead>
+                  <tr>
+                    <th class="product-image">Imagen</th>
+                    <th class="product-name">Producto</th>
+                    <th class="product-price">Precio</th>
+                    <th class="product-quantity">Cantidad</th>
+                    <th class="product-total">Total</th>
+                    <th class="product-remove">Remover</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="product-image">
+                      <img src="public/images/celular.jpg" alt="Image" >
+                    </td>
+                    <td class="product-name">
+                      <h2 class="h5 text-black">Smartphone</h2>
+                    </td>
+                    <td>$49.00</td>
+                    <td class="centered">
+                         <div class="row">
+                            <div class="col s2">
+                                <button class="btn ">-</button>   
+                            </div>   
+                            <div class="col s2 ">                 
+                                <input type="text" class="text-center" value="1">  
+                            </div> 
+                            <div class="col s2">                      
+                            <button class="btn">+</button>
+                            </div> 
+                        </div>
+                    </td>
+                    <td>$49.00</td>
+                    <td><a href="#" class="btn">X</a></td>
+                  </tr>
+
+                  
+                </tbody>
+              </table>
+              <br>
+              <div class="row">
+                <div clas="col s3">
+                    <button class="btn">Borrar Todo</button>
+                </div>
+                
+                <div clas="col s3">
+                    <button class="btn">Comprar</button>
+                </div>
+                
+              </div>
+            
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--<div class=container>
     <a> UN COLABORADOR MODIFICO ALGO </a>
 </div>
+<div id="cualquiercosa">
+    <div v-for="n in milista">
+        <a class="btn">
+            {{n}}
+        </a>
+    </div>
+    
+</div>
+
+<script>
+    new Vue({
+        el: '#cualquiercosa',
+        data: {
+            milista: ['peras', 'manzana', 'uva']
+        }
+    })
+
+</script>-->
