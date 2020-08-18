57436c2342649677f680c571dfa2b3c10d0dd02f
nickienicks <69782181+nickienicks@users.noreply.github.com>
2020-08-17 21:55:11 -0500

    up

--- app/Views/pages/detalles.php
+++ app/Views/pages/detalles.php
@@ -1,9 +1,22 @@
+<style>
+
+#imagen_central{
+justify-content: center;
+
+}
+.cuadro_detalle{
+    margin:40px;
+}
 
 
+</style>
 <div class="container" style="display:block">
-    <div class="row">
-        <div class=" col s6" styles="justify-content=center">
+    <div class="cuadro_detalle">
+    <div class="row ">
+        <div id="imagen_central" class="col s6">
+            
             <img src="public/images/celular.jpg" alt="">
+
         </div>
         <div class="col s6">
             <div>
@@ -17,35 +30,36 @@
                 <h6>Precio:</h6><p>$50</p>
                 <form action="#">
                 <div class="row">
-                    <div class="col s3">
+                    
                         <p>
                             <label>
                                 <input type="radio" name="opcion"class="with-gap"/>
                                 <span>Black</span>
                             </label>
                         </p>
-                    </div>
-                    <div class="col s3">
+                    
+                    
                         <p>
                             <label>
                                 <input type="radio" name="opcion"class="with-gap"/>
                                 <span>White</span>
                             </label>
                         </p>
-                    </div>
-                    <div class="col s6">
+                    
+                    
                         <p>
                             <label>
                                 <input type="radio" name="opcion"class="with-gap"/>
                                 <span>Electric Purpple</span>
                             </label>
                         </p>
-                    </div>
+                    
                 
                 </div>
                 </form>
             </div>
             
         </div>
+    </div>
     </div>
 </div>
