ce4bf88b51ddad62c201264a02a885d3792f0bd1
CoffeeDuck <davidlive0159@gmail.com>
2020-09-03 03:01:22 -0500

    servicios usuarios

--- app/Controllers/AdmUsuarios.php
+++ app/Controllers/AdmUsuarios.php
@@ -1,8 +1,21 @@
 <?php namespace App\Controllers;
+use App\Models\QueryUsuarios;
 class AdmUsuarios extends BaseController
 {
 	public function Usuarios_Listar()
 	{
 		echo $this->layout_view('admin', 'pages/adm_usuarios');
+	}
+	public function serv_Usuario_Listar()
+	{
+		$_usuario = new QueryUsuarios();
+		$_V = [
+			'dni' => '',
+			'cargo' => '',
+			'usuario' => ''
+		];
+		$_V = array_merge($_V, $_POST);
+		$res = $_usuario->Usuarios_Listar($_V['dni'], $_V['cargo'], $_V['usuario']);
+		return $this->response->setJSON($res);
 	}
 }
--- app/Views/pages/adm_usuarios.php
+++ app/Views/pages/adm_usuarios.php
@@ -1,15 +1,46 @@
+<style media="screen">
+	.radiogroup{
+		background: white;
+		margin: 1px 0;
+		padding: 16px 8px;
+		position: relative;
+		display: block;
+		cursor: pointer;
+	}
+</style>
 <div id="app" class="module">
-	<main-search notadd namemodule="REGISTRO DE VENTAS">
+	<main-search notadd namemodule="Lista de usuarios">
 
 	</main-search>
 	<Soption>
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		<div class="c"><i class="fal icon-pres c fa-user"></i> <p>Filtra los usuarios del sistema segun sus roles</p></div>
+		<div class="space-32"></div>
+		<div class="w100">
+				<label class="radiogroup">
+					<input name="group1" type="radio"  value="1" v-model="filter_cargo">
+					<span>ADMINISTRADOR</span>
+				</label>
+				<label class="radiogroup">
+					<input name="group1" type="radio" value="4" v-model="filter_cargo">
+					<span>USUARIOS</span>
+				</label>
+				<label class="radiogroup">
+					<input name="group1" type="radio" value="2" v-model="filter_cargo">
+					<span>VENDEDOR</span>
+				</label>
+				<label class="radiogroup">
+					<input name="group1" type="radio" name="group1" value="3"  v-model="filter_cargo">
+					<span>ALMACENERO </span>
+				</label>
+		</div>
 	</Soption>
 
 </div>
 <script>
 new Vue({
 	el: '#app',
-	data: {}
+	data: {
+		filter_cargo: '4'
+	}
 });
 </script>
